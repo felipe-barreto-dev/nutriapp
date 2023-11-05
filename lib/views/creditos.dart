77f2b259a74085effaa6ca770a2e99effe01dcc9
Rafael Ramiro <93105946+RMRafael@users.noreply.github.com>
2023-11-05 10:46:38 -0300

    Update creditos.dart
    Interface changes.

--- lib/views/creditos.dart
+++ lib/views/creditos.dart
@@ -1,82 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:nutriapp/main.dart';
 import 'package:nutriapp/util/custom_drawer.dart';
-import 'package:nutriapp/views/cadastros/cadastro_usuario.dart';
 
 class Creditos extends StatefulWidget {
-  const Creditos({super.key});
+  const Creditos({Key? key});
 
   @override
   CreditosState createState() => CreditosState();
 }
 
 class CreditosState extends State<Creditos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const CustomDrawer(currentIndex: 1),
+      appBar: AppBar(
+        title: const Text("Créditos"),
+        backgroundColor: const Color.fromARGB(255, 131, 196, 181),
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.asset(
+              'assets/images/logo+name.png'), // Substitua pelo caminho da sua imagem
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Projeto desenvolvido para a disciplina de Programação para Dispositivos Móveis. Professor: Luiz Felipe.\nIntegrantes do grupo:',
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 18, // Ajuste o tamanho da fonte conforme desejado
+                color: const Color.fromARGB(255, 40, 106, 86),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          CreditItem(name: 'Breno Mazzini Costa'),
+          CreditItem(name: 'Felipe Barreto Pereira'),
+          CreditItem(name: 'Gabrielle de Oliveira Bussi'),
+          CreditItem(name: 'Luiz Felipe Barbosa Arruda'),
+          CreditItem(name: 'Millena de Souza Netto'),
+          CreditItem(name: 'Rafael Claro Ramiro'),
+        ],
+      ),
+    );
+  }
+}
+
+class CreditItem extends StatelessWidget {
+  final String name;
+
+  const CreditItem({required this.name});
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Créditos',
-      home: Scaffold(
-        drawer: const CustomDrawer(currentIndex: 1),
-        appBar: AppBar(
-          title: const Text("Créditos"),
-          backgroundColor: const Color.fromARGB(255, 131, 196, 181),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Breno Mazzini Costa',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Felipe Barreto Pereira',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Gabrielle de Oliveira Bussi',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Luiz Felipe Barbosa Arruda',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Millena de Souza Netto',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Rafael Claro Ramiro',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)
-              ),
-            ],
+    return ListTile(
+      title: Center(
+        child: Text(
+          name,
+          textAlign: TextAlign.justify,
+          style: TextStyle(
+            fontSize: 18,
+            color: const Color.fromARGB(255, 131, 196, 181),
+            fontWeight: FontWeight.bold,
           ),
-        )
+        ),
       ),
     );
   }
